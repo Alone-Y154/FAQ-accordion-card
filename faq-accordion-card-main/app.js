91e7e399d6984e43ffbb4f5ef3819d0cab100358
Yashwanth Krishna <72192888+Alone-Y154@users.noreply.github.com>
2023-08-12 23:14:19 +0530

    directory delete

--- faq-accordion-card-main/app.js
+++
@@ -1,8 +0,0 @@
-    let divs = document.querySelectorAll('.faq');
-
-
-    divs.forEach((div, i) => {
-      div.addEventListener('click',() => {
-        div.classList.toggle("faq-question-click");
-      })
-    });
